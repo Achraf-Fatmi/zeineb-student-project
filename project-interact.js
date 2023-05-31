013f80e01f50d8b1e0104a37f1d54afcfd598789
Achraf-Fatmi <fatmiachraf@gmail.com>
2023-05-31 15:50:37 +0100

    some addons

---
+++ project-interact.js
@@ -0,0 +1,37 @@
+
+
+// const butt=document.getElementsByClassName('grif');
+// const navi=document.getElementsByClassName('nav')
+// for(let i of butt) {
+//     i.addEventListener('click', creat1, function thered() {
+//         document.navi.classList.add('red')
+//     })
+// }
+function griffon(){
+    alert("Congratulations you are a Gryffindor!");
+    const para = document.createElement("p");
+const node = document.createTextNode("This is new.");
+para.appendChild(node);
+
+const element = document.getElementById("div1");
+element.innerHTML=''
+element.appendChild(para);
+};
+// function creat1(){
+// var firstd =document.createElement("div");
+// var texty=document.createTextNode("this is a something griffy");
+// firstd.appendChild(texty);
+// var hiss = documet.getElementsByClassName("houses");
+// hiss.appendChild(firstd);
+
+// }
+function raven(){
+    alert("Congratulations you are a Gryffindor!");
+    const para = document.createElement("p");
+const node = document.createTextNode("This is new2.");
+para.appendChild(node);
+
+const element = document.getElementById("div1");
+element.innerHTML='';
+element.appendChild(para);
+};
